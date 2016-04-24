34eacbb759c0e469dc0f02adbd4bb9941e31d178
rayschneider <schneider.ray@gmail.com>
2016-04-24 12:39:39 -0400

    Updated the tool considerably to include job costs, and to make it generally less shit than it was.

--- Pricing.R
+++ Pricing.R
@@ -1,20 +1,36 @@
-# set your directory below to where you saved the two .scv files from the repo. 
+# Declare your directory and files here.  You also declare your system for 
+#  market prices and build indices
 
-setwd("C:/Users/Ray/EveData/T3")
+directory <- "C:/Users/Ray/EveData/T3"
+typeIDList <-"typelist.csv"
+buildReqsList <- "tierone.csv"
+marketSystem <- "30000142" # Market Prices, currently Jita
+buildSystem <- "Paara"     # Indices for builds
+
+# Declare some things needed later that might need updating
+CRESTurl <- "https://public-crest.eveonline.com/market/prices/"
+eveIndURL <- "http://api.eve-industry.org/system-cost-index.xml?name="
+
+# Import needed files
+setwd(directory)
+typefile <- paste(directory,typeIDList, sep = "/")
+tierone <- paste(directory,buildReqsList, sep = "/")
+
+# Load required packages
 library("XML")
 library("dplyr")
+library("jsonlite")
 
-# Getting shit
-#url <- "http://api.eve-central.com/api/marketstat?typeid=30466&usesystem=30000142"
+# Getting prices from the Eve-Central API
+#   url <- "http://api.eve-central.com/api/marketstat?typeid=30466&usesystem=30000142"
 
-typefile <- "C:/Users/Ray/EveData/T3/typelist.csv"
 prices <- NULL
 typelist <- read.csv(typefile)
 types <- as.list(typelist[,1])
 
 getTypeId <- function(typeid) {
         type <- as.character(typeid)
-        system <- "30000142"
+        system <- marketSystem # This is Jita
         initstr <- "http://api.eve-central.com/api/marketstat?typeid="
         endstr <- "&usesystem="
         url <- paste(c(initstr,type,endstr,system), sep = "", collapse = "")
@@ -27,32 +43,75 @@ getTypeId <- function(typeid) {
 
 lapply(types,getTypeId)
 
+# Get a list of the adjusted Prices from the CREST API
+getCRESTadjPrice <- function() {
+        rawPrices <- fromJSON(CRESTurl, simplifyDataFrame = TRUE)
+        a <- data.frame(rawPrices)
+        b <- a[,c(2,4)]
+        c <- as.data.frame.table(b)
+        d <- c[,c(6,7,3,5)]
+        adjPrice <- unique(d)
+        rm(a,b,c,d)
+        colnames(adjPrice) <- c("typeID","typeName","adjustedPrice","CRESThref")
+        adjPrice <<- adjPrice
+}
+
+getCRESTadjPrice()
+
+adjPrice <- adjPrice[,c(1,3)]
+
+# Get System indices from eve-industry.org
+
+sysAPIurl <- paste(eveIndURL,buildSystem, sep = '')
+sysCost1 <- xmlParse(sysAPIurl)
+sysCostData <- xmlToList(sysCost1)
+
+indices <- data.frame(system = character(),
+                      systemID = integer(),
+                      manufacturing = numeric(),
+                      TE = numeric(),
+                      ME = numeric(),
+                      copy = numeric(),
+                      revEng = numeric(),
+                      inv= numeric(),
+                      stringsAsFactors = FALSE)
+indices[1,1] <- sysCostData[[1]][[7]][[2]]
+indices[1,2] <- as.numeric(sysCostData[[1]][[7]][[1]])
+indices[1,3] <- as.numeric(sysCostData[[1]][[1]][[1]])
+indices[1,4] <- as.numeric(sysCostData[[1]][[2]][[1]])
+indices[1,5] <- as.numeric(sysCostData[[1]][[3]][[1]])
+indices[1,6] <- as.numeric(sysCostData[[1]][[4]][[1]])
+indices[1,7] <- as.numeric(sysCostData[[1]][[5]][[1]])
+indices[1,8] <- as.numeric(sysCostData[[1]][[6]][[1]])
+
 # Tier one build costing stuffs
-
-tierone <- "C:/Users/Ray/EveData/T3/tierone.csv"
 primary <- read.csv(tierone)
-
 primary$buildcost <- 0
-asd <- c("marketstat.type..attrs", "marketstat.type.sell.percentile","marketstat.type.sell.volume","marketstat.type.buy.volume")
-pricesimple <- subset(prices, select = asd)
-pnames <- c("typeID","sellpercent","sellVol","buyVol")
-colnames(pricesimple) <- pnames
+uglyNames <- c("marketstat.type..attrs","marketstat.type.sell.percentile","marketstat.type.buy.percentile","marketstat.type.sell.volume","marketstat.type.buy.volume")
+pricesimple <- subset(prices, select = uglyNames)
+prettyNames <- c("typeID","sellPercent","buyPrecent","sellVol","buyVol")
+colnames(pricesimple) <- prettyNames
 
 # Merge in "tier one" build prices - Building just from the first level down of sold mats.
 mp <- merge(x = primary, y=pricesimple, by.x = "materialTypeID", by.y="typeID", all.x=TRUE )
-mp$buildcost <- mp$matQty * as.numeric(mp$sellpercent) / mp$quantity
+mp <- merge(x= mp, y = adjPrice, by.x = "materialTypeID", by.y="typeID", all.x = TRUE)
+mp$materialCost <- mp$matQty * as.numeric(mp$sellPercent) / mp$quantity
+mp$jobFee <- mp$matQty * mp$adjustedPrice * indices[1,3]
+mp$fullCost <- mp$materialCost + mp$jobFee
 
 # Aggregate by typeID
-mpagg <- aggregate(mp$buildcost, by=list(category=mp$typeID), FUN=sum)
+mpagg <- aggregate(mp$fullCost, by=list(category=mp$typeID), FUN=sum)
 colnames(mpagg) <- c("typeID","cost")
 mpaggm <- merge(x = mpagg, y = pricesimple, by = "typeID", all.x = TRUE)
 
 # Caluclate Profit
-mpaggm$profit <- as.numeric(mpaggm$sellpercent) - mpaggm$cost
+mpaggm$profit <- as.numeric(mpaggm$sellPercent) - mpaggm$cost
+
+# Add names
 fnames <- typelist[,1:2]
 profit <- merge(mpaggm, fnames, by = "typeID", all.x=TRUE)
-profit$margin <- profit$profit / profit$cost * 100
-
+profit$margin <- profit$profit / profit$cost 
+profit <- profit[,c(8,1,7,9,5,6,2,3,4)]
 # Raw Isk Profit
 rawprofit <- arrange(profit, desc(profit))
 
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# T3-Build-Tool-Prototype-
+# T3-Build-Tool-Prototype
+
+Currently this tool calculates the profit for building various T3 items based on buying the materials for a given item at the current Jita Sell rate, then building and selling the item to the same Sell rate.
+
+Market data is from Eve-central API.
+Build fee data is derived from CREST and the Eve-industry APIs. 
+
+To use this tool:
+
+1. Download everything into a folder
+2. Open the file "Pricing.R", either in text editor or R Studio
+3. Update the first five variables.  If all you are doing is using the program as configured fot Atlas, you only need to update the single "directory variable.
+4. Save the program.
+5. Run/source the program.  If using Rstudio, check "Source on Svae" to ensure this happens.  The program takes about 1-3 minutes to run, depending.
+6. Upon completion use either of the functions printRawProfits() or printMarginProfits() to a get a list of the most profitbale items by raw or marginal isk vale.  The argument for both functions is the number of rows you want returned.  printRawProfit(20) will give the 20 most profitable items.
+
